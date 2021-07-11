62fc040519f5516bdc9cda1af4d4c8819f8ac21c
Roberto Buzzini <roberto.buzzini95@gmail.com>
2021-07-11 16:56:36 +0200

    Some files deleted

---
+++ Scripts/alice_words.py
@@ -0,0 +1,19 @@
+text = open(r"txt\alice_in_wonderland.txt")
+print(text)
+
+text = text.split()
+
+len(text)
+"""
+import random
+print("Hello World")
+print(random.choice(["carta", "forbice", "sasso"]))
+p = [1, 2, 3]
+
+#def count_num(l):
+h = "ciao"
+
+def count_numbers(list):
+    return len(list)
+print(count_numbers([1,4,2,5,7,3]))
+"""
