9a33f305ffd85f65bad0dad0a85a40552bda672d
Roberto Buzzini <roberto.buzzini95@gmail.com>
2021-10-17 19:01:14 +0200

    Python tip added

---
+++ Tips/polymorphism example.py
@@ -0,0 +1,12 @@
+# Polymorphism is a key concept in Object Oriented Programming.
+# It literally means "multiple forms". It refers to use a single type entity
+# to represent different types in different scenarios.
+
+# Let's see a simple example with a built-in function
+name = 'Joseph' # string
+len(name)
+
+names = ['Joseph', 'Jane'] # list
+len(names)
+# That's polymorphism in action: a single function knows how to handle different
+# kind of objects.
