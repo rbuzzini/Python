cf97646549804abffbc1195b0e34bb1c56de8c77
Roberto Buzzini <roberto.buzzini95@gmail.com>
2021-10-24 14:15:48 +0200

    OOP item project added

--- Fun Projects/Exercises/main.py
+++ Fun Projects/Exercises/main.py
@@ -1,10 +1,15 @@
-from item import Item
-from phone import Phone
-from keyboard import Keyboard
-import csv
 import os
 
 os.getcwd()
+os.chdir(r'C:\Users\rbuzzini\Documents\Personale\Git\Python\Fun Projects\Exercises')
+
+from item import Item
+os.chdir(r'C:\Users\rbuzzini\Documents\Personale\Git\Python\Fun Projects\Exercises')
+from phone import Phone
+os.chdir(r'C:\Users\rbuzzini\Documents\Personale\Git\Python\Fun Projects\Exercises')
+from keyboard import Keyboard
+import csv
+
 items_path = r'C:\Users\rbuzzini\Documents\Personale\Git\Python\Fun Projects\datasets'
 os.chdir(items_path)
 
