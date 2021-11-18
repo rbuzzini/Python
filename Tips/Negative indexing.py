6d62bf461bd8d3f762c9c16ab6dd2d61a37cdaa3
Roberto Buzzini <roberto.buzzini95@gmail.com>
2021-11-18 14:56:32 +0100

    Python tip added

---
+++ Tips/Negative indexing.py
@@ -0,0 +1,8 @@
+# In Python you can use negative indexing to access objects from the end. 
+# While positive index starts with 0, negative index starts with -1.
+
+name = "Alexander"
+print(name[0])
+print(name[-1])
+print(name[0:3])
+print(name[-1:-4:-1])
