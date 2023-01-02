8848a76b5e70f25e6fcc40807d604f43cc0344eb
Roberto Buzzini <roberto.buzzini95@gmail.com>
2023-01-02 17:11:34 +0100

    Added Pearson's Chi Square test tip

--- Tips/Hypothesis tests/Chi Square test.py
+++ Tips/Hypothesis tests/Chi Square test.py
@@ -19,6 +19,14 @@ chi2_contingency(tbl)
 # - The 1st value is the statistic
 # - The 2nd value is the p-value = 0.84 ==> null hypothesis not rejected
 # - The 3rd value represents the degree of freedom (row_num - 1) * (col_num - 1)
-# - The matrix represents the theoric frequencies if the variables are independent.
+# - The matrix represents the theoric frequencies if the variables are independent
+# (the expected frequencies).
 # Every theoric frequency is equal to the row_sum * col_sum / total_sum
 # The test compare these values with the observed values.
+# If expected and observed frequencies are equal ==> statistic = 0 and p-value=1
+# If all expected frequencies are > 30 this test is universally accepted, if
+# they are not the situation is too approximated so the p-value isn't reliable
+
+# When the contingency table is 2x2 the Fosher's exact test is more appropriated
+# (it isn't affected by Pearson's chi square approximation).
+
